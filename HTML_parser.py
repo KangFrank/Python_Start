927f3f292d42179391e1a5f4d3195d3752256789
KangFrank <kangliangshu@163.com>
2017-07-08 22:53:09 +0800

    Add files via upload

---
+++ HTML_parser.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python3
+#-*-coding:utf-8 -*-
+#Filename: HTML_parser
+
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+#Define the HTML parser class
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self,tag,attrs):
+        print('<%s>'%tag)
+    def handle_endtag(self,tag):
+        print('</%s>'%tag)
+    def handle_startendtag(self,tag,attrs):
+        print('<%s/>'%tag)
+    def handle_data(self,data):
+        print(data)
+    def handle_comment(self,data):
+        print('<!--',data,'-->')
+    def handle_entityref(self,name):
+        print('&%s;'%name)
+    def handle_charref(self,name):
+        print('&#%s;'%name)
+
+parser=MyHTMLParser()
+parser.feed('''
+<html>
+<head></head>
+<body>
+<!--test html parser -->
+<p>Some <a href=\"#\">html</a> HTML&nbsp;tutorial...<br>END</p>
+</body></html>
+''')
